cd94f0f6ef356ad214fa27ecac24695c9439c86a
hello-mr-vishu <vishnurongali21@gmail.com>
2026-03-26 21:01:49 +0530

    feat: add blog post configurations and their associated build output logs.

--- src/assets/configs/blogConfig.js
+++ src/assets/configs/blogConfig.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { AiFillGithub } from "react-icons/ai";
 import { SiMedium } from "react-icons/si";
-import fastapiSklearn from "../images/fastapi_sklearn.png";
 
 const blogConfig = [
   {
